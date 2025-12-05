89bd2280542c8efbaf0d5ad2de872bff3e209ca7
Leopold Johannes Meinel <leo@meinel.dev>
2025-12-05 15:14:20 +0100

    cfg_attrs: Add attrs used in bevy_new_2d

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,10 @@
 //!
 //! Heavily inspired by: <https://github.com/TheBevyFlock/bevy_new_2d>
 
+// Support configuring Bevy lints within code.
+#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
+// Disable console on Windows for non-dev builds.
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+
 /// Main function
 fn main() {}
