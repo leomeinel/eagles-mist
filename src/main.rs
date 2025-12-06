db7fbf72e7267a556abbaff6c5c6841fe15bdbf0
Leopold Johannes Meinel <leo@meinel.dev>
2025-12-06 10:38:19 +0100

    Credits: Use consistent format across repos

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@
  * Copyright (c) 2025 Leopold Johannes Meinel & contributors
  * SPDX ID: Apache-2.0
  * URL: https://www.apache.org/licenses/LICENSE-2.0
+ * -----
+ * Heavily inspired by: https://github.com/TheBevyFlock/bevy_new_2d
  */
 
 //! Main with [`AppPlugin`]
-//!
-//! Heavily inspired by: <https://github.com/TheBevyFlock/bevy_new_2d>
 
 // Support configuring Bevy lints within code.
 #![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
