c2e0e49a081cdb4ee8c45af4990581cd0a7c0390
Leopold Johannes Meinel <leo@meinel.dev>
2026-04-29 23:38:03 +0200

    comments: Remove copyright headers
    I no longer want to use copyright headers. I had to modify them in most
    of my projects multiple times over the last years.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 /*
- * File: main.rs
- * Author: Leopold Johannes Meinel (leo@meinel.dev)
- * -----
- * Copyright (c) 2025 Leopold Johannes Meinel & contributors
- * SPDX ID: Apache-2.0
- * URL: https://www.apache.org/licenses/LICENSE-2.0
- * -----
- * Heavily inspired by: https://github.com/TheBevyFlock/bevy_new_2d
+ * Heavily inspired by:
+ * - https://github.com/TheBevyFlock/bevy_new_2d
  */
 
 //! Main with [`AppPlugin`]
